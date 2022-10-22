42a32b35f16adba41e1f8e8a81333f1305225fb4
amir <nagat1700@gmail.com>
2022-10-22 23:28:29 +0300

    update

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -2,9 +2,9 @@ import React from "react";
 // import useStorage from "../hooks/useStorage";
 import useStorage from '../hooks/useStorage';
 
-const ProgressBar =({file,setFile})=>{
-    const {url,progress} = useStorage(file)
-    console.log(progress,url);
+const ProgressBar =({file,setFile})=>{ 
+    const {url,progress} = useStorage(file) 
+    console.log(progress,url); 
     return(
         <div className="prgress-bar">progress</div>
     )
